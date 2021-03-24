225413c41ddb4b3427123efacc511fba4d12161c
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2021-03-24 23:50:48 +0100

    Tested on different machine, initialization correct

--- src/main/java/com/io/petclinic/PetclinicApplication.java
+++ src/main/java/com/io/petclinic/PetclinicApplication.java
@@ -8,7 +8,7 @@ public class PetclinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetclinicApplication.class, args);
-		System.out.println("no to w droge");
+		System.out.println("Welcome to Shih Tzoo");
 	}
 
 }
