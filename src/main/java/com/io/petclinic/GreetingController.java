ae1913db44ec4d680455cfe097a5351fd7890615
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-03-25 12:09:02 +0100

    Request test

--- src/main/java/com/io/petclinic/GreetingController.java
+++ src/main/java/com/io/petclinic/GreetingController.java
@@ -13,6 +13,7 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        System.out.println("anotni");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
