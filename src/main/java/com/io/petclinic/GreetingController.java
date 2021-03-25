fe6ea9a670363d25b9dd695511c33800dc96b6f6
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2021-03-25 13:11:15 +0100

    cross origin v2

--- src/main/java/com/io/petclinic/GreetingController.java
+++ src/main/java/com/io/petclinic/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@CrossOrigin(origins = "", allowedHeaders = "")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 
 @RestController
 public class GreetingController {
