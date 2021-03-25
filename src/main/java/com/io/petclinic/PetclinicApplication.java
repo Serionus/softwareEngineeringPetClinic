bb10f8cbd0a50c030d0572359176228abeaf7592
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-03-25 11:42:47 +0100

    Heroku prepared

--- src/main/java/com/io/petclinic/PetclinicApplication.java
+++ src/main/java/com/io/petclinic/PetclinicApplication.java
@@ -8,7 +8,7 @@ public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
-		System.out.println("Shih Tzoo plz help");
+		System.out.println("Shih Tzoo plz help jeuz chryste");
 	}
 
 }
