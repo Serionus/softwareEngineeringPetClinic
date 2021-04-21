ade65d55770277caad3fef2ef30721602b986729
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2021-04-21 21:34:39 +0200

    methods in visit service - retreiving visits

--- src/main/java/com/io/petclinic/model/services/VisitService.java
+++ src/main/java/com/io/petclinic/model/services/VisitService.java
@@ -1,13 +1,49 @@
 package com.io.petclinic.model.services;
 
+import com.io.petclinic.exceptions.OwnerNotFoundException;
+import com.io.petclinic.exceptions.VetNotFoundException;
+import com.io.petclinic.model.entities.Owner;
+import com.io.petclinic.model.entities.Pet;
+import com.io.petclinic.model.entities.Vet;
+import com.io.petclinic.model.entities.Visit;
+import com.io.petclinic.model.repositories.OwnerRepository;
+import com.io.petclinic.model.repositories.PetRepository;
+import com.io.petclinic.model.repositories.VetRepository;
 import com.io.petclinic.model.repositories.VisitRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VisitService {
     private final VisitRepository visitRepository;
+    private final OwnerRepository ownerRepository;
+    private final VetRepository vetRepository;
+    private final PetRepository petRepository;
 
-    public VisitService(VisitRepository repository) {
+    public VisitService(VisitRepository repository, OwnerRepository ownerRepository, VetRepository vetRepository, PetRepository petRepository) {
         this.visitRepository = repository;
+        this.ownerRepository = ownerRepository;
+        this.vetRepository = vetRepository;
+        this.petRepository = petRepository;
     }
+
+    public List<Visit> getAllOwnerVisits(Long ownerId) {
+        Owner wantedOwner = ownerRepository.findById(ownerId)
+                .orElseThrow( () -> new OwnerNotFoundException(ownerId));
+        List<Pet> pets = wantedOwner.getPets();
+        for (Pet pet: pets) {
+            return pet.getVisits();
+        }
+        return null;
+    }
+
+
+    // czy potrzebujemy tego? czy lepiej jedna metoda na zasadzie userId
+    // zamiast X metod do petId, ownerId i vetId
+//    public List<Visit> getAllVetVisits(Long vetId) {
+//        Vet wantedVet = vetRepository.findById(vetId)
+//                .orElseThrow( () -> new VetNotFoundException(vetId));
+//        return wantedVet.getVisits();
+//    }
 }
