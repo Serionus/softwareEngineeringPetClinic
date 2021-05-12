b297e7d13c9a428ef8966819e546f48f843222ab
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-05-12 23:56:10 +0200

    Testing D T O pets

--- src/main/java/com/io/petclinic/controllers/PetController.java
+++ src/main/java/com/io/petclinic/controllers/PetController.java
@@ -1,10 +1,14 @@
 package com.io.petclinic.controllers;
 
 
+import com.io.petclinic.controllers.entities.HumanDTO;
 import com.io.petclinic.controllers.entities.PetDTO;
+import com.io.petclinic.model.entities.Owner;
+import com.io.petclinic.model.entities.Pet;
 import com.io.petclinic.model.repositories.PetRepository;
 import com.io.petclinic.model.services.PetService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +28,11 @@ public class PetController {
         return (List<PetDTO>) petService.findAllPets().stream().map(pet -> new PetDTO(pet.getName(), pet.getSpecies()));
     }
 
-//    @PostMapping("/owners/{id}/pets/")
-//    Pet pet
+    @GetMapping("/pets/{id}")
+    public PetDTO getPet(@PathVariable Long id){
+        Pet pet = petService.findPet(id);
+        return new PetDTO(pet.getName(), pet.getSpecies());
+    }
 
 }
 
