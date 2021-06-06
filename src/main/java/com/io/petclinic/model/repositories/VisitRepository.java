a7e4ae96bdfa4d583ef1d6fdff412d210192808d
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2021-06-06 17:38:44 +0200

    Ongoing fight with VisitService and its tests

--- src/main/java/com/io/petclinic/model/repositories/VisitRepository.java
+++ src/main/java/com/io/petclinic/model/repositories/VisitRepository.java
@@ -7,7 +7,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface VisitRepository extends JpaRepository<Visit, Long> {
+
+    // sprawdź czy istnieją już zarezerwowane wizyty w przedziale czasowym wizyty do umówieniafin
     List<Visit> findAllByBeginTimeAfterAndEndTimeBefore(LocalDateTime beginTime, LocalDateTime endTime);
+
+    // umawiamy dwójkę między 1 i 3
+    //  endtime1 < begintime2-endtime2 < begintime3
+//    List<Visit> findAllByEndTimeAfterAndBeginTimeBefore(LocalDateTime endTime, LocalDateTime beginTime);
     List<Visit> findAllByPetPetId(Long petId);
     List<Visit> findAllByVetVetId(Long vetId);
+    // sprawdzenie tego nam nic nie daje bo to tylko sprawdza czy w ogóle istnieją wizyty poza ramami
+    // czasowymi tego co chcemy umówić
+//    List<Visit> findAllByBeginTimeBeforeAndEndTimeAfter();
+
 }
